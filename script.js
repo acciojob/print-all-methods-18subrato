4f90486e8688e0ab0c699e1480315771e0567fe7
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-16 13:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function allMethods() {
-	 const mathMethods = Object.getOwnProperyNames(Math).filter((prop)=>typeof Math[prop] === 'function');
+	 const mathMethods = Object.getOwnPropertyNames(Math).filter((prop)=>typeof Math[prop] === 'function');
 	return mathMethods;
 }
 
